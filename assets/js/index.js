945b7324543196b1cb0dc26068b25332114f4555
Gildman Laércio <laerciokarlagabm@gmail.com>
2025-09-03 12:34:29 -0300

    index.js fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,30 +3,14 @@ const dessertSection = document.getElementById("desserts");
 const cartSection = document.getElementById("cart-section");
 const orderConfirmation = document.getElementById("order-confirmation");
 
-function showSection(sectionToShow) {
-  [dessertSection, cartSection, orderConfirmation].forEach(section => {
-    section.classList.remove("active");
-  });
-
-  sectionToShow.classList.add("active");
-}
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   showSection(dessertSection);
-// });
-
 confirmButton.addEventListener("click", () => {
-  // dessertSection.style.display = "none";
-  // cartSection.style.display = "none";
-  // orderConfirmation.style.display = "flex";
-  showSection(orderConfirmation);
+  dessertSection.style.display = "none";
+  cartSection.style.display = "none";
+  orderConfirmation.style.display = "flex";
 });
 
 function startNewOrder() {
-  // orderConfirmation.style.display = "none";
-  // dessertSection.style.display = "";
-  // cartSection.style.display = "";
-  cart = [];
-  updateCart();
-  showSection(dessertSection);
+  orderConfirmation.style.display = "none";
+  dessertSection.style.display = "";
+  cartSection.style.display = "";
 }
